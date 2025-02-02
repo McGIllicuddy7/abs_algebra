742e93b10b32b3f0079d9e2fd6e4ce3cb86e874c
Bridget Hartin Pasco <isaacpasco@icloud.com>
2025-02-02 01:08:52 -0800

    halllp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::num::ParseIntError;
+
 use numbers::*;
 use rand::{rng, Rng};
 
@@ -5,14 +7,27 @@ pub mod numbers;
 fn main() {
     let mut count = 0;
     let mut average_counts =0;
-    for i in 0..10_000{
-        let fl:f64 =rng().random();
+    let mut dels = 0.0;
+    for _ in 0..5{
+        let mut fl:f64 =rng().random();
+        let mut il:i64 = rng().random();
+        il = il.abs()%32;
+        il =0;
+        fl += il as f64;
+        fl += 0.5;
         let (v, n) = Q::from_f64_prof(fl);
-        assert!((f64::from(v)-fl).abs()<0.000001);
-        println!("{i}");
+        let del =  (f64::from(v)-fl).abs();
+        dels += del;
+        if del>0.0001{
+            println!("del:{del}, fl:{fl}, v_value:{}, v:{:#?}",f64::from(v), v);
+        }
         count += 1;
         average_counts += n;
     }
-    println!("on average took: {} iterations", average_counts as f64/ (count as f64));
-    println!("{:#?}",Q::from(1./3.));
+    println!("on average took: {} iterations, {} delta", average_counts as f64/ (count as f64), dels/(count as f64));
+     let (q,_) = Q::from_f64_prof(8./100.);
+     println!("q:{:#?}\nf:{}",q,f64::from(q));
+    let q1 = Q::new(1,2);
+    let q2 = Q::new(3,2);
+    println!("q1:{:#?}, q2:{:#?}, q1+q2{:#?}", q1, q2, q1+q2);
 }
--- src/numbers.rs
+++ src/numbers.rs
@@ -128,44 +128,61 @@ impl Identity for i8{
         return 1;
     }
 }
-pub fn gcd<T:Div<Output = T>+Rem<Output = T>+Ring+PartialOrd>(a:T, b:T)->T{
+pub fn gcd<T:Div<Output = T>+Rem<Output = T>+Ring+PartialOrd>(a0:T, b0:T)->T{
+    let mut a = a0;
+    let mut b = b0;
     if b>a{
         return gcd(b,a);
-    } else if a== b{
+    }    
+    else if a== b{
         return a;
     }
-    let r = a%b.clone();
-    if r == T::zero(){
-        return b;
-    } else{
-        return gcd(b, r);
+    loop{
+        let r = a.clone()%b.clone();
+        if r == T::zero(){
+            return b;
+        } else{
+            a = b;
+            b = r;
+        }
     }
+
 }
 
 #[derive(Debug,Clone, Copy)]
 pub struct Q{
-    pub num:i32,
-    pub denum:i32,
+    pub num:i64,
+    pub denum:i64,
 }
 
 impl Q{
     pub fn simplify(&self)->Self{
         let num =self.num;
         let denum =self.denum;
-        let g = gcd(num, denum);
+        let g = 1;//gcd(num, denum);
         Self { num: num/g, denum:denum/g } 
     }
     pub fn new(num:i32, denum:i32)->Self{
+        let g = gcd(num.abs(), denum);
+        assert!(g>0);
+        Self { num: (num/g)as i64, denum:(denum/g)as i64 }
+    }
+    pub fn new_i64(num:i64, denum:i64)->Self{
         let g = gcd(num, denum);
-        Self { num: num/g, denum:denum/g }
+        assert!(g>0);
+        assert!(num>0);
+        assert!(denum>0);
+        Self { num: (num/g)as i64, denum:(denum/g)as i64 } 
     }
     pub fn inverse(&self)->Self{
         return Self { num: self.denum, denum: self.num }
     }
     pub fn from_f64_prof(value:f64)->(Self, u64){
         let sign = value<0.0;
-        let info=  (format!("{}",value).len()-1) as i32;
-        let v = value.abs();
+        let v = (value.abs()- value.abs().floor()).abs();
+        let mut info=  (format!("{}",v).len()) as i32;
+        let ipart = Self{num:(value-v) as i64, denum:1};
+       // println!("value: {value},ipart:{:#?}", ipart);
         let mut out = Self{num:1, denum:1};
         let mut diff = (f64::from(out) -v).abs();
         let mut iters =0;
@@ -173,14 +190,20 @@ impl Q{
             if diff == 0.0{
                 break;
             }
-            let min = (v*(j as f64)-1.0).floor()as i32;
-            let max =(v*(j as f64)+1.0).ceil()as i32; 
-            for i in min..max+1{
+            let mut min = (v*(j as f64)-1.0).floor()as i32;
+            if min <= 0{
+                min = 1;
+            }
+            let mut  max =(v*(j as f64)+1.0).ceil()as i32; 
+            if max <= 0{
+                max = 1;
+            }
+            for i in min..max{
                 iters += 1;
                 if (i-1) as f64/(j as f64)>v{
                     break;
                 }
-                let tmp = Self{num:i,denum:j};
+                let tmp = Self::new(i, j);
                 let d = (f64::from(tmp)-v).abs();
                 if d<diff{
                     diff = d;
@@ -188,23 +211,27 @@ impl Q{
                 }
             }
         }
-        if sign{
-            (Self::new(out.num *-1, out.denum),iters)
+        out = Self::new_i64(out.num+ipart.num*out.denum, out.denum);
+        out = if sign{
+            Self::new((out.num*-1) as i32, (out.denum) as i32)
         } else{
-            (Self::new(out.num , out.denum),iters)
-        }
+            Self::new((out.num) as i32 , (out.denum) as i32)
+        };
+        (out,iters)
     }
 }
 impl Add for Q{
     type Output = Self;
     fn add(self, rhs: Self) -> Self {
-        Self::new(self.num*rhs.denum+rhs.num*self.denum,self.denum*rhs.denum)
+        assert!(self.denum>0 && self.num>0 && rhs.denum>0 && rhs.num>0);
+        let out = Self::new_i64(self.num*rhs.denum+rhs.num*self.denum,rhs.denum*self.denum);
+        out
     }
 }
 impl Mul for Q{
     type Output = Self;
     fn mul(self, rhs: Self) -> Self::Output {
-        Self::new(self.num*rhs.num, self.denum*rhs.denum)
+        Self::new((self.num*rhs.num) as i32, (self.denum*rhs.denum) as i32)
     }
 }
 impl Div for Q{
@@ -222,18 +249,21 @@ impl From<Q> for f64{
 impl From<f64> for Q{
     fn from(value: f64) -> Self {
         let sign = value<0.0;
+        let info=  (format!("{}",value).len()-1) as i32;
         let v = value.abs();
         let mut out = Self{num:1, denum:1};
         let mut diff = (f64::from(out) -v).abs();
-        for j in 1..i32::MAX{
+        for j in i32::MAX-info..i32::MAX{
             if diff == 0.0{
                 break;
             }
-            for i in 0..i32::MAX{
+            let min = (v*(j as f64)-1.0).floor()as i32;
+            let max =(v*(j as f64)+1.0).ceil()as i32; 
+            for i in min..max+1{
                 if (i-1) as f64/(j as f64)>v{
                     break;
                 }
-                let tmp = Self{num:i,denum:j};
+                let tmp = Self{num:i as i64,denum:j as i64};
                 let d = (f64::from(tmp)-v).abs();
                 if d<diff{
                     diff = d;
@@ -242,9 +272,9 @@ impl From<f64> for Q{
             }
         }
         if sign{
-            Self::new(out.num *-1, out.denum)
+            Self::new((out.num *-1) as i32, (out.denum) as i32)
         } else{
-            Self::new(out.num , out.denum)
+            Self::new((out.num) as i32 , (out.denum) as i32)
         }
     }
 }
